67ae1cf6e6fad4bcc714567ffe053b79af004762
buschidos <sergey.snjozyk@gmail.com>
2014-12-08 18:13:27 +0200

    almost done

--- js/common.js
+++ js/common.js
@@ -2,6 +2,7 @@ head.ready(function() {
 
     $('body').on('click', function() {
         $('.lang__list').slideUp(200);
+        $('.popup').fadeOut(200);
     });
 
     $('.lang').on('click', function(event) {
@@ -14,23 +15,61 @@ head.ready(function() {
         event.stopPropagation();
     });
 
-    // $('.menu-btn').on('click', function(event) {
-    //     $(this).toggleClass('is-active');
-    //     $('.header__menu').toggleClass('is-visible');
-    // });
+    $('.menu-btn').on('click', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        toggleMenu();
+    });
+
+    $('.js-show-popup').on('click', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        var popup = $('#' + $(this).data('popup'));
+        popup.fadeIn(200);
+    });
+
+    $('.popup__close').on('click', function(event) {
+        event.preventDefault();
+        var popup = $(this).parents('.popup');
+        popup.fadeOut(200);
+    });
+
+    $('.popup__inner').on('click', function(event) {
+        event.stopPropagation();
+    });
+
+    $('.popup').on('click', function(event) {
+        $(this).fadeOut(200);
+    });
+
+
+
+    // functions
 
     var toggleMenu = function() {
         $('.menu-btn').toggleClass('is-active');
         $('.header__menu').toggleClass('is-visible');
         $('body').toggleClass('no-scroll');
-        $('.overlay').fadeToggle(200);
+        toggleOverlay();
     };
 
-    $('.menu-btn').on('click', function(event) {
-        event.preventDefault();
-        toggleMenu();
-    });
+    var toggleOverlay = function() {
+        var overlay = $('.overlay');
+        if ( !overlay.is(':visible') ) {
+            overlay.fadeIn(200);
+        } else {
+            overlay.fadeOut(200);
+        }
+    };
 
-
+    var toggleBodyScroll = function () {
+        var body        = $('body');
+        var targetClass = 'no-scroll';
+        if (body.hasClass(targetClass)) {
+            body.removeClass(targetClass);
+        } else {
+            body.addClass(targetClass);
+        }
+    };
 
 });
