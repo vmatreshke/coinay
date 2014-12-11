2784795b722ebecb011de0f27b102182f1afd3bf
buschidos <sergey.snjozyk@gmail.com>
2014-12-11 16:00:20 +0200

    added infoWindow for each point on map

--- js/map.js
+++ js/map.js
@@ -1,60 +1,95 @@
 var map;
-    function initialize() {
+var pointsOnMap = [
+        [40.4153871, -3.7081734, 1, {
+            'head'    : 'First point',
+            'address' : 'address 1',
+            'tel'     : '0800-800-800',
+            'open'    : '8:30AM - 5:00PM',
+            'common'  : 'some text'
 
-      var mapOptions = {
-        zoom: 17,
-        disableDefaultUI: true,
-        scrollwheel: false,
-        center: new google.maps.LatLng(40.4149025,-3.7037002)
-      };
-      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        }],
+        [40.4150604, -3.7062288, 2, {
+            'head'    : 'Second point long name',
+            'address' : 'address long address',
+            'tel'     : '0800-800-800',
+            'open'    : '8:30AM - 5:00PM',
+            'common'  : 'some text'
 
-      var image = 'img/marker.png';
+        }],
+        [40.4153886, -3.7033086, 3, {
+            'head'    : 'Third point very long long long name ',
+            'address' : 'address long address address long address',
+            'tel'     : '0800-800-800',
+            'open'    : '8:30AM - 5:00PM',
+            'common'  : 'some text'
 
-      var points = [
-        ['First point',  40.4153871, -3.7081734, 1],
-        ['Second point', 40.4150604, -3.7062288, 2],
-        ['Third point',  40.4153886, -3.7033086, 3],
-      ];
+        }],
+        [40.4163886, -3.7053086, 3, {
+            'head'    : 'Point very long long name ',
+            'address' : '',
+            'tel'     : '0800-800-800',
+            'open'    : '8:30AM - 5:00PM',
+            'common'  : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe repudiandae laborum harum quibusdam officiis eos optio ipsum. Tempora nisi numquam doloribus, sit itaque, unde deleniti molestiae qui explicabo corporis, ad.'
 
-      function setMarkers(map, locations) {
-        for (var i = 0; i <= locations.length; i++) {
-          var point = locations[i];
-          var myLatlng = new google.maps.LatLng(point[1], point[2]);
-          var marker = new google.maps.Marker({
-            position : myLatlng,
-            map: map,
-            icon: image,
-            title: point[0],
-            zIndex: point[3]
-          });
+        }]
+    ];
+
+// Function return array of markers that was create from "locations" and added to "map"
+function setMarkers(map, locations) {
+        var markers = [];
+        var image = new google.maps.MarkerImage('img/marker2x.png', null, null, null, new google.maps.Size(55,20));
+        for (var i = 0; i < locations.length; i++) {
+            var point    = locations[i];
+            var myLatlng = new google.maps.LatLng(point[0], point[1]);
+            var marker   = new google.maps.Marker({
+                position : myLatlng,
+                map      : map,
+                icon     : image,
+                title    : point[3].head,
+                zIndex   : point[2]
+            });
+            marker.infoContent = point[3];
+            markers.push(marker);
         }
-      }
-
-      // google.maps.event.addListener(map, 'mouseover', function(event) {
-      //   if (window.innerWidth > 800) {
-      //     window.setTimeout(function() {
-      //       map.setOptions({
-      //         disableDefaultUI: false,
-      //         scrollwheel: true
-      //       });
-      //     }, 1000);
-      //   }
-      // });
-
-      // google.maps.event.addListener(map, 'mouseout', function(event) {
-      //   if (window.innerWidth > 800) {
-      //     window.setTimeout(function() {
-      //       map.setOptions({
-      //         disableDefaultUI: true,
-      //         scrollwheel: false
-      //       });
-      //     }, 1000);
-      //   }
-      // });
-
-      setMarkers(map, points);
-
+        return markers;
     }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+// After function is complete all marker in array will contain object with info for infowindow
+function setInfoWindowContent(arrayOfMarkers, infoWindow) {
+        for (var i = 0; i < arrayOfMarkers.length; i++) {
+            google.maps.event.addListener(arrayOfMarkers[i], 'click', function() {
+                var content = composeInfoWindowContent(this.infoContent);
+                infoWindow.setContent(content);
+                infoWindow.open(map, this);
+            });
+        }
+    }
+
+function composeInfoWindowContent(data) {
+    return '<ul class="marker-info">' +
+                '<li class="marker-info__head">'     + data.head    + '</li>' +
+                '<li class="marker-info__address">'  + data.address + '</li>' +
+                '<li class="marker-info__tel">'      + data.tel     + '</li>' +
+                '<li class="marker-info__open">'     + data.open    + '</li>' +
+                '<li class="marker-info__common">'   + data.common  + '</li>' +
+            '</ul>';
+    }
+
+function initialize() {
+    var mapOptions = {
+            zoom: 17,
+            disableDefaultUI: true,
+            scrollwheel: false,
+            center: new google.maps.LatLng(40.4149025,-3.7037002)
+        };
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    var mapMarkers = setMarkers(map, pointsOnMap);
+
+    var mapInfoWindow = new google.maps.InfoWindow();
+
+    setInfoWindowContent(mapMarkers, mapInfoWindow);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
